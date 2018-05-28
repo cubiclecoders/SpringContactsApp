3402d7d3c5c18a1ff63eeb9f0e93f4d878b258b9
rsl <rsl@rsls-air>
2018-05-28 15:18:51 +0530

    Greeting Message Done

--- SpringContactsApp/src/main/java/com/ravi/contactsapp/test/TestClass.java
+++ SpringContactsApp/src/main/java/com/ravi/contactsapp/test/TestClass.java
@@ -4,15 +4,19 @@ public class TestClass {
 
 	public static void main(String[] args) {
 
-
-		//ApplicationContext ctx = new ClassPathXmlApplicationContext("contactsapp-servlet_test.xml");
+		// ApplicationContext ctx = new
+		// ClassPathXmlApplicationContext("contactsapp-servlet_test.xml");
 
 		Integer[] nameInt = { 5, 6, 3, 9, 9, 10 };
-		System.out.println(nameInt);
-        System.out.println("Build Hello Message First");
+	
+		String gudMorning="Good Morning ";
 
-		System.out.println("Hello Message");
+	String hello="Hello,";
 
+		for (Integer iValue : nameInt) {
+			System.out.println(hello+gudMorning +iValue);
+
+		}
 
 	}
 
