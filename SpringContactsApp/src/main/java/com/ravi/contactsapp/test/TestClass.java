ccaa7ab18758958f1de2a99fe03aed625865e458
rsl <rsl@rsls-air>
2018-05-28 13:31:04 +0530

    Test Class CheckIn

--- SpringContactsApp/src/main/java/com/ravi/contactsapp/test/TestClass.java
+++ SpringContactsApp/src/main/java/com/ravi/contactsapp/test/TestClass.java
@@ -7,10 +7,11 @@ public class TestClass {
 
 	public static void main(String[] args) {
 
-		ApplicationContext ctx = new ClassPathXmlApplicationContext("contactsapp-servlet_test.xml");
+		//ApplicationContext ctx = new ClassPathXmlApplicationContext("contactsapp-servlet_test.xml");
 
 		Integer[] nameInt = { 5, 6, 3, 9, 9, 10 };
 		System.out.println(nameInt);
+		System.out.println("Hello Git");
 
 	}
 
